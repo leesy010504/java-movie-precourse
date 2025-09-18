ca3d00cad0679911aa07417b53f0a2f776670fe3
이상윤 <sang-yoon@isang-yun-ui-MacBookPro.local>
2025-09-19 05:29:19 +0900

    feat(seat): getter 추가, contains 연산을 위한 Override
    좌석의 위치가 같다면 같은 객체로 판단하고, equals에서 사용한 필드를 똑같이 사용하여 해시코드를 생성합니다.

--- src/main/java/com/example/precourse/seat/entity/SeatEntity.java
+++ src/main/java/com/example/precourse/seat/entity/SeatEntity.java
@@ -1,5 +1,7 @@
 package com.example.precourse.seat.entity;
 
+import java.util.Objects;
+
 public class SeatEntity {
     private final String location;
     private final SeatGrade grade;
@@ -11,5 +13,22 @@ public class SeatEntity {
 
     public SeatGrade getGrade() {
         return grade;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SeatEntity seat = (SeatEntity) o;
+        return Objects.equals(location, seat.location);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(location);
     }
 }
