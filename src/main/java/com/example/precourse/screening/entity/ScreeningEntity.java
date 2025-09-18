5677ff471c058c0f83957cb270bf27983ee6ce62
이상윤 <sang-yoon@isang-yun-ui-MacBookPro.local>
2025-09-19 05:31:22 +0900

    feat(screening): 예매 서비스에 필요한 메서드들과 getter 추가

--- src/main/java/com/example/precourse/screening/entity/ScreeningEntity.java
+++ src/main/java/com/example/precourse/screening/entity/ScreeningEntity.java
@@ -12,15 +12,39 @@ public class ScreeningEntity {
     private MovieEntity movie;
     private LocalDateTime startTime;
     private TheaterEntity theater;
-    private List<SeatEntity> bookedSeats = new ArrayList<>();
+    private final List<SeatEntity> availableSeats;
 
-    public ScreeningEntity(MovieEntity movie,  LocalDateTime startTime, TheaterEntity theater) {
+    public ScreeningEntity(MovieEntity movie,  LocalDateTime startTime, TheaterEntity theater,  List<SeatEntity> availableSeats) {
         this.movie = movie;
         this.startTime = startTime;
         this.theater = theater;
+        this.availableSeats = new ArrayList<>(availableSeats);
     }
 
     public LocalDateTime getStartTime() {
         return this.startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return this.startTime.plusMinutes(movie.getRunningTime());
+    }
+
+    public boolean isWithinOperatingHours() {
+        return theater.isWithinOperatingHours(startTime, getEndTime());
+    }
+
+    public boolean overlapsWith(ScreeningEntity other) {
+        return this.getEndTime().isAfter(other.getStartTime()) &&
+                this.getStartTime().isBefore(other.getEndTime());
+    }
+
+    public void reserveSeats(List<SeatEntity> seatsToReserve) {
+        for (SeatEntity seatToReserve : seatsToReserve) {
+            if (!this.availableSeats.contains(seatToReserve)) {
+                throw new IllegalArgumentException("선택한 좌석 " + seatToReserve.getLocation() + "는 이미 예매되었거나 존재하지 않습니다.");
+            }
+        }
+
+        this.availableSeats.removeAll(seatsToReserve);
     }
 }
